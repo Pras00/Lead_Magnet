e3da0175f1f7fc760fd9d7a5029addbec11e3888
sulthony.akbar <sulthonyakbar18@gmail.com>
2024-03-15 08:21:44 +0700

    [FEAT] First Upload

---
+++ lead-magnet-sales-page/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Sales Page - Lead Magnet</title>
+</head>
+
+<body>
+
+    <!-- headline -->
+    <div class="container-lg bg-slate-900">
+        <div id="headline" class="flex items-center justify-center h-screen px-12">
+            <h1 class="poppins-bold text-white text-center font-3rem">
+                Dapatkan Rahasia Sukses Bisnis Anda! Peroleh Panduan Eksklusif Gratis untuk Membangun Produk yang Laku!
+            </h1>
+        </div>
+        <div id="headline-caption" class="flex flex-col items-center justify-center">
+            <h3 class="poppins-semibold text-white text-center font-1-5rem px-10">
+                Gain the Rights to Re-Sell This Bundle Under Your Own Name and Brand for Your Personal - Massive Profits with Quotes Buddy 2.0!
+            </h3>
+            <div class="border-0 bg-orange-600 rounded-2xl px-12 py-5 my-16 mx-8">
+                <h3 class="poppins-bold font-1-5rem text-center text-white">
+                    Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                </h3>
+            </div>
+
+            <img src="https://placehold.co/600x600" alt="">
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-3 gap-4 my-16 mx-4 md:mx-8 lg:mx-16">
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-last">
+                        <i class="fas fa-users text-white fa-2x ms-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-first">
+                        <i class="fas fa-users text-white fa-2x me-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-last">
+                        <i class="fas fa-users text-white fa-2x ms-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-first">
+                        <i class="fas fa-users text-white fa-2x me-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-last">
+                        <i class="fas fa-users text-white fa-2x ms-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-first">
+                        <i class="fas fa-users text-white fa-2x me-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+            </div>
+
+            <h1 class="poppins-bold text-white text-center font-2rem mb-5">Grab This Offer for 50% OFF!</h1>
+            <button id="btn-payment" type="button" class="text-white bg-orange-600 hover:bg-bg-yellow-600 poppins-bold font-1-5rem rounded-full py-5 dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mb-5">
+                <i class="fas fa-cart-arrow-down me-5"></i>
+                Get Instant Access Now
+            </button>
+            <img id="guarantee" src="img/30-guarantee.webp" alt="" class="mb-16">
+        </div>
+    </div>
+
+    <div class="container-lg bg-slate-200">
+        <div class="flex flex-col items-center justify-center pt-16">
+            <h1 class="poppins-bold text-center font-2rem w-3/4">
+                Make Your Social Media Stand Out with
+                Quotes Buddy 2.0 Creative Content
+            </h1>
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-3 gap-4 my-16 mx-4 md:mx-8 lg:mx-16">
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-last">
+                        <i class="fas fa-users text-white fa-2x ms-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-first">
+                        <i class="fas fa-users text-white fa-2x me-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-last">
+                        <i class="fas fa-users text-white fa-2x ms-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-first">
+                        <i class="fas fa-users text-white fa-2x me-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-last">
+                        <i class="fas fa-users text-white fa-2x ms-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+                <div class="border-0 bg-orange-600 rounded-2xl px-4 md:px-6 lg:px-8 py-5 w-96 flex items-center justify-start">
+                    <div class="flex items-center justify-center order-first">
+                        <i class="fas fa-users text-white fa-2x me-5"></i>
+                    </div>
+                    <h3 class="poppins-bold font-1rem text-center text-white">
+                        Everything is ALL Yours! - You can do anything with this AMAZING Quote Designs Collection
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-lg bg-slate-900">
+        <div class="flex flex-col items-center justify-center h-screen">
+            <h1 class="poppins-bold text-white text-center font-3rem">
+                Going Live - Saturday, 10th March 2024 <br>09:00 AM EST <br>
+                <span class="text-orange-600">Don't Miss It!</span>
+            </h1>
+
+            <div class="grid grid-cols-4 grid-rows-1 gap-4 my-14 bg-white">
+                <div class="text-center text-white">
+                    <div class="days poppins-extrabold text-orange-600 text-4xl"></div>
+                    <div class="poppins-bold text-orange-600">Days</div>
+                </div>
+                <div class="text-center text-white">
+                    <div class="hours poppins-extrabold text-orange-600 text-4xl"></div>
+                    <div class="poppins-bold text-orange-600">Hours</div>
+                </div>
+                <div class="text-center text-white">
+                    <div class="minutes poppins-extrabold text-orange-600 text-4xl"></div>
+                    <div class="poppins-bold text-orange-600">Minutes</div>
+                </div>
+                <div class="text-center text-white">
+                    <div class="seconds poppins-extrabold text-orange-600 text-4xl"></div>
+                    <div class="poppins-bold text-orange-600">Seconds</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function startCountdown() {
+            const targetDate = new Date("March 31, 2024 09:00:00").getTime();
+
+            const x = setInterval(function() {
+                const now = new Date().getTime();
+
+                const distance = targetDate - now;
+
+                const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                document.querySelector('.days').innerHTML = days;
+                document.querySelector('.hours').innerHTML = hours;
+                document.querySelector('.minutes').innerHTML = minutes;
+                document.querySelector('.seconds').innerHTML = seconds;
+
+                if (distance < 0) {
+                    clearInterval(x);
+                    document.querySelector('.container-lg').innerHTML = "EXPIRED";
+                }
+            }, 1000);
+        }
+
+        window.onload = startCountdown;
+    </script>
+
+</body>
+
+</html>
